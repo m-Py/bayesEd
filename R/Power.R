bf814f12ee1708ab8f000d0db5103ce05e1fe56b
Martin Papenberg <martin.papenberg@hhu.de>
2018-11-08 17:27:57 +0100

    initial code commit

---
+++ R/Power.R
@@ -0,0 +1,234 @@
+
+#' Compute statistical "power" for the default t-test Bayes factor
+#'
+#' For a given N, this function returns the Bayes factor that will be
+#' exceeded with a specified probability in a t-test. Transfers the
+#' frequentist concept of statistical power to Bayes factors. Uses the
+#' default Bayesian t test implemented in the package `BayesFactor`. 
+#' 
+#' @param N The total sample size over both groups in the independent
+#'     groups t test.
+#' @param effect_size The assumed effect size d.
+#' @param nsim The number of simulations that are conducted to determine
+#'     the "power" of the t test. Increase this number to obtain a more
+#'     precise estimate.
+#' @param rscale The scaling parameter in the Cauchy prior used in the
+#'     Bayes factor computation. Defaults to `sqrt(2) / 2`.
+#' @param probability A scalar indicating the "power". See `Details`.
+#' @param say_result Boolean. Print a verbal description of the
+#'     computation when it is finished?
+#' 
+#' @details For Bayes factors, there is no concept of statistical
+#'     power. However, for a given N and effect size, we can investigate
+#'     the distribution of Bayes factors. Here, "power" then means the
+#'     probability that an observed Bayes factor is at least as high as
+#'     a particular value. We specify this "power" using the parameter
+#'     `probability`. In case of a null-effect, "power" is the
+#'     probability that an observed Bayes factor is smaller than a
+#'     particular value. The function does not analytically determine
+#'     "power", but uses simulation to generate the distribution of
+#'     Bayes factors.
+#' 
+#' @examples
+#' ## N is the total sample size across two groups
+#' power_bf(N = 200, effect_size = 0.4)
+#' ## Null effect:
+#' power_bf(N = 1000, effect_size = 0)
+#' ## Small effect size:
+#' power_bf(N = 1000, effect_size = 0.2)
+#' ## Vary width of the prior distribution for the alternative hypothesis:
+#' power_bf(N = 1000, effect_size = 0.2, rscale = 0.2)
+#' 
+#' @references
+#'
+#' Morey, R. D., & Rouder, J. N. (2015). BayesFactor: Computation of
+#'     bayes factors for common designs. Retrieved from
+#'     https://CRAN.R-project.org/package=BayesFactor
+#'
+#' Rouder, J. N., Speckman, P. L., Sun, D., Morey, R. D., & Iverson,
+#'     G. (2009). Bayesian t tests for accepting and rejecting the null
+#'     hypothesis. Psychonomic Bulletin & Review, 16(2), 225–237.
+#'
+#' @export
+#' 
+
+power_bf <- function(N, effect_size, nsim = 1000, rscale = sqrt(2)/2,
+                     probability = 0.8, say_result = TRUE) {
+  if (effect_size > 0) {
+    power <- 1 - probability
+    direction <- "larger"
+  } else if (effect_size == 0) {
+    power <- probability
+    direction <- "smaller"
+  } else {
+    stop("effect size cannot be lower than 0")
+  }
+  info <- BF_quantiles(estimate_expected_bf(effect_size, N, nsim, rscale), power)
+  info$BF <- exp(info$logBF) # other functions return the log BF
+  info$quantile <- NULL
+  info$power    <- probability
+  if (say_result) {
+    cat("\n Approximately", probability*100,
+        "% of all Bayes factors will be", direction, "than",
+        round(info$BF, 2), "\n\n")
+  }
+  return(info)
+}
+
+#' Simulate the distribution of the t-test Bayes factor
+#'
+#' Uses the default Bayes factor employed in the BayesFactor package.
+#'
+#' @param effect_size The assumed effect size d.
+#' @param sample_sizes A vector of sample sizes to be used in the
+#'     simulation. Means the total sample sizes, i.e., the sample sizes
+#'     across the two groups in the t test.
+#' @param n_bayes_factors How many Bayes factor should be computed. Is
+#'     replicated for each of the elements in the vector `sample_sizes`.
+#' @param rscale The scaling parameter in the Cauchy prior used in the
+#'     Bayes factor computation. Defaults to `sqrt(2) / 2`.
+#'
+#' @return A data.frame in long format where each row represents the
+#'     simulation of a t test. Each row has four columns: `logBF` - the
+#'     natural logarithm of the Bayes factor; `N` - the total sample
+#'     size in the t test; `eff_size` - the effect size; `rscale` - the
+#'     scaling parameter of the prior distribution.
+#' 
+#' @references
+#'
+#' Morey, R. D., & Rouder, J. N. (2015). BayesFactor: Computation of
+#'     bayes factors for common designs. Retrieved from
+#'     https://CRAN.R-project.org/package=BayesFactor
+#'
+#' Rouder, J. N., Speckman, P. L., Sun, D., Morey, R. D., & Iverson,
+#'     G. (2009). Bayesian t tests for accepting and rejecting the null
+#'     hypothesis. Psychonomic Bulletin & Review, 16(2), 225–237.
+#'
+#' @examples 
+#' bfs <- estimate_expected_bf(effect_size = 0.5, sample_sizes = seq(50, 350, by = 50), n_bayes_factors = 300)
+#'
+#' ## Use `exp` to obtain the "normal" BF that is not logarithmic
+#' tapply(exp(bfs$logBF), bfs$N, median)
+#' 
+#' ## Compare "power" between different sample sizes:
+#' power <- 0.8
+#' tapply(exp(bfs$logBF), bfs$N, quantile,  probs = 1 - power)
+#'
+#' 
+#' @export
+#' 
+#' @importFrom BayesFactor ttestBF
+#' @importFrom BayesFactor extractBF
+#' 
+estimate_expected_bf <- function(effect_size, sample_sizes,
+                                 n_bayes_factors, rscale=sqrt(2)/2) {
+  ## store BFs for each repetition for one sample size:
+  repetitions_bf <- vector(length = n_bayes_factors)
+  ## store BFs by sample size:
+  samples_bf     <- list()
+  for (i in 1:length(sample_sizes)) {
+    for (j in 1:n_bayes_factors) {
+      group0 <- rnorm(sample_sizes[i]/2, 0, 1)
+      group1 <- rnorm(sample_sizes[i]/2, effect_size, 1)
+      tmpBF  <- BayesFactor::ttestBF(group0, group1, rscale = rscale)
+      repetitions_bf[j] <- BayesFactor::extractBF(tmpBF)$bf
+    }
+    ## store all bayes factors for sample size i (store the log BF!)
+    samples_bf[[i]] <- log(repetitions_bf)
+  }
+  ## convert to data.frame in long format:
+  ret <- data.frame(logBF = unlist(samples_bf),
+                    N  = rep(sample_sizes, each = n_bayes_factors),
+                    eff_size = effect_size,
+                    rscale = rscale)
+  return(ret)
+}
+
+#' Compute quantiles for the distribution of Bayes factors
+#'
+#' @param dat A `data.frame` returned by `estimate_expected_bf`
+#' 
+#' @importFrom reshape2 melt
+#' @importFrom plyr adply
+BF_quantiles <- function(dat, quantiles = c(0.2, 0.5, 0.8)) {
+  ## obtain quantiles of log BF by sample size
+  arr <- tapply(dat$logBF, dat$N, quantile, probs = quantiles,
+                simplify = FALSE)
+  
+  ## use plyr to convert array to data.frame
+  dfw <- plyr::adply(arr, 1)
+  ## use reshape to create long data
+  long_quantiles <- reshape2::melt(dfw, id.vars = c("X1"), factorsAsStrings = TRUE)
+  
+  ## Some renaming:
+  long_quantiles$N <- as.numeric(levels(long_quantiles$X1))[long_quantiles$X1]
+  long_quantiles$X1 <- NULL
+  long_quantiles$quantile <- long_quantiles$variable
+  long_quantiles$variable <- NULL
+  long_quantiles$logBF    <- long_quantiles$value
+  long_quantiles$value    <- NULL
+  
+  ## Attach effect size and prior scale
+  long_quantiles$eff_size <- unique(dat$eff_size)
+  long_quantiles$rscale   <- unique(dat$rscale)
+  return(long_quantiles)
+}
+
+plot_BF_quantiles <- function(BF_quantiles, thresholds=NULL, ylim=NULL,
+                              main = "", axis = c("log", "standard")) {
+  
+  ## first make some adjustments to the plot in dependence of which
+  ## axis the user wants to show:
+  ylab <- ""
+  ## adjust margin to make space for a second y-axis -- ‘c(bottom,
+  ## left, top, right)’; The default ‘c(5, 4, 4, 2) + 0.1’.
+  def_mar <- c(5, 4, 4, 2) + 0.1
+  cur_mar <- def_mar
+  if ("standard" %in% axis) {
+    cur_mar <- cur_mar + c(0,0,0,2)
+    par(mar = cur_mar)
+  }
+  else {
+    cur_mar <- cur_mar + c(0,0,0,-2)
+    par(mar = cur_mar)
+  }
+  if ("log" %in% axis) {
+    draw_y <- "s"
+    ylab <- "log(BF10)"
+  } else {
+    draw_y <- "n"
+    cur_mar <- cur_mar + c(0,-2,0,0)
+    par(mar = cur_mar)
+  }
+  
+  ## draw the actual plot
+  plot(BF_quantiles$N, BF_quantiles$logBF, pch = 3,
+       col = rep(1:length(unique(BF_quantiles$quantile)),
+                 each = length(unique(BF_quantiles$N))), ylab = ylab,
+       xlab = "", las = 1, ylim = ylim, main = main,
+       yaxt = draw_y)
+  
+  ## add y-axis to the right if that was required by the user
+  if ("standard" %in% axis) 
+    add_normal_bf_scale(1)
+  
+  ## indicate the neutral evidence line if it was required by the user
+  cols = rep("darkgrey", length(thresholds))
+  cols[which(thresholds == 1)] <- "black"
+  abline(h = log(thresholds), lty = 2, lwd = 1.5, col = cols)
+  
+  ## Label x-axis
+  axis(1, 20, "n", tick = FALSE)
+  
+  ## reset margins
+  on.exit(par(mar = def_mar))
+}
+
+add_normal_bf_scale <- function(by = 1) {
+  label_bf_lim <- seq(-30, 30, by = by)
+  labs <- round(exp(label_bf_lim), 2)
+  labs[labs >= 10] <- round(labs[labs >= 10])
+  labs[labs >= 1] <- round(labs[labs >= 1], 1)
+  axis(4, at = label_bf_lim, labels = labs, las = 1)
+  mtext("BF10", 4, line = 3.1)
+}
---
+++ R/Visualizations.R
@@ -0,0 +1,320 @@
+
+## Author: Martin Papenberg, inspired from a blog post by Jeff Rouder 
+## (http://jeffrouder.blogspot.com/2016/01/what-priors-should-i-use-part-i.html) 
+
+## This file contains functions to visualize Bayes factors (prior and
+## marginal likelihoods), and computes t-test Bayes factors for the
+## comparison of two group means. The code is meant for educational
+## purposes only; to actually compute Bayes factors, I encourage the use
+## of the `BayesFactor` package.
+
+#' Visualize a prior on Cohen's d for a Bayesian t-test
+#' 
+#' @param alternative A function object. The default is a Cauchy prior
+#'     with scaling parameter `sqrt(2) / 2` as is the default in package
+#'     `BayesFactor` (Morey & Rouder, 2015). This argument can also be a
+#'     scalar number, in which case it is assumed that the alternative
+#'     is a point hypothesis on Cohen's d (with Cohen's d = `prior`).
+#' @param null Boolean. Plot a line on x = 0 representing the null
+#'     hypothesis?
+#' @param from the left margin of the x-axis
+#' @param to the right margin of the x-axis
+#' @param xlab the label of the x-axis
+#' @param frame.plot Should a frame be drawn?
+#' @param col The color of the curve of the alternative hypothesis.
+#' @param lwd The line width
+#' @param ... additional parameters passed to function `curve`
+#' 
+#' @examples
+#' ## Standard cauchy prior:
+#' visualize_prior(lty = 2, lwd = 2)
+#' ## Wide cauchy prior:
+#' visualize_prior(function(x) dcauchy(x, scale = 1))
+#' ## Normal prior with M = 0 and SD = 0.4
+#' visualize_prior(function(x) dnorm(x, 0, 0.4))
+#' ## Point prior with Cohen's d = 0.4
+#' visualize_prior(0.4)
+#'
+#' @references
+#'
+#' Morey, R. D., & Rouder, J. N. (2015). BayesFactor: Computation of
+#'     bayes factors for common designs. Retrieved from
+#'     https://CRAN.R-project.org/package=BayesFactor
+#'
+#' Rouder, J. N., Speckman, P. L., Sun, D., Morey, R. D., & Iverson,
+#'     G. (2009). Bayesian t tests for accepting and rejecting the null
+#'     hypothesis. Psychonomic Bulletin & Review, 16(2), 225–237.
+#'
+#' @export
+#' 
+visualize_prior <- function(alternative = function(x) dcauchy(x, scale = sqrt(2) / 2),
+                            null = TRUE, from = -4, to = 4,
+                            xlab = "Cohen's d", frame.plot = FALSE,
+                            col = "red", lwd = 3, ...) {
+  if (class(alternative) != "function") {
+    plot(c(alternative, alternative), c(0, 1), type = "l",
+         las = 1, xlim = c(from, to), xlab = xlab, 
+         frame.plot = frame.plot, yaxt = "n", col = "transparent", 
+         ylab = "")
+    abline(v = alternative, col = col, lwd = lwd)
+  } else {
+    curve(alternative, las = 1, from = from, to = to, xlab = xlab, 
+          frame.plot = frame.plot, yaxt = "n", col = col, ylab = "", 
+          lwd = lwd, ...)
+  }
+  if (null) {
+    abline(v = 0, col = "#005A31", lwd = lwd)
+    legend("topleft", lwd = lwd, legend = c("Null", "Alternative"), 
+           cex = 1.2, col = c("#005A31", "red"), lty = c(2,2), bty = "n")
+  }
+}
+
+
+#' Visualize predictions of null and alternative hypothesis for a
+#' Bayesian two-group t-test
+#' 
+#' By specifying an observed effect (t-value) one can also obtain an
+#' illustration of the ratio of the marginal likelihoods, i.e., an
+#' illustration of the Bayes factor
+#' 
+#' @param alternative A function object. The default is a Cauchy prior
+#'     with scaling parameter `sqrt(2) / 2` as is the default in package
+#'     `BayesFactor` (Morey & Rouder, 2015). This argument can also be a
+#'     scalar number, in which case it is assumed that the alternative
+#'     is a point hypothesis on Cohen's d (with Cohen's d = `prior`).
+#'     If `alternative` is NULL, only the null hypothesis predictions
+#'     are drawn.
+#' @param n1 The sample size in group 1
+#' @param n2 The sample size in group 2
+#' @param from the left margin of the x-axis
+#' @param to the right margin of the x-axis
+#' @param lty The line type; lty[1] specifies the line type for the null
+#'     hypothesis; lty[2] specifies the line type for the alternative
+#'     hypothesis; lty[3] specifies the line type for the alternative
+#'     hypothesis (only has an effect if an observed effect size is
+#'     specified via argument `observed_t`)
+#' @param col Specify the coloring of the plot. col[1] specifies the
+#'     color for the null hypothesis; lty[2] specifies the color for the
+#'     alternative hypothesis; lty[3] specifies the color for the line
+#'     the illustrates the observed effect (only has an effect if an
+#'     observed effect size is specified via argument `observed_t`);
+#'     lty[4] specifies the color of the points at the intersection of
+#'     the observed effect and the curves for the hypotheses (only has
+#'     an effect if an observed effect size is specified via argument
+#'     `observed_t`)
+#' @param observed_t An observed t-value. Does not need to be specified
+#'     and defaults to NULL. If this argument is passed, the marginal
+#'     likelihoods of the null and the alternative hypothesis are drawn
+#'     and a Bayes factor is displayed.
+#' @param frame.plot Should a frame be drawn?
+#' @param xlab The label of the x-axis
+#' @param BFx The x coordinate where the Bayes factor is displayed in
+#'     the plot. Only has an effect if an `observed_t` is passed.
+#' @param BFy The y coordinate where the Bayes factor is displayed in
+#'     the plot. Only has an effect if an `observed_t` is passed.
+#' @param BF10 Boolean; defaults to TRUE. Should a Bayes factor be
+#'     displayed quantifying the evidence in favor of the alternative
+#'     hypothesis? The BF01 is displayed if `BF10` is `FALSE`.  Only has
+#'     an effect if an `observed_t` is passed.
+#' @param legend_placement A keyword passed to `legend` to indicate
+#'     where the legend has to be placed. Defaults to "topleft".  Only
+#'     has an effect if an `observed_t` is passed.
+#' @param ... additional parameters passed to `curve`
+#'
+#' @references
+#'
+#' Morey, R. D., & Rouder, J. N. (2015). BayesFactor: Computation of
+#'     bayes factors for common designs. Retrieved from
+#'     https://CRAN.R-project.org/package=BayesFactor
+#'
+#' Rouder, J. N., Speckman, P. L., Sun, D., Morey, R. D., & Iverson,
+#' G. (2009). Bayesian t tests for accepting and rejecting the null
+#' hypothesis. Psychonomic Bulletin & Review, 16(2), 225–237.
+#' 
+#' @examples 
+#' ## Using the default cauchy prior
+#' visualize_predictions(n1 = 100, n2 = 100, observed_t = 3, lty = c(1, 1, 2))
+#' ## Using a wide Cauchy prior
+#' visualize_predictions(function(x) dcauchy(x, scale = 1), 
+#' n1 = 100, n2 = 100, observed_t = 3, lty = c(1, 1, 2))
+#' ## Using a normal prior (M = 0, SD = 0.4)
+#' visualize_predictions(function(x) dnorm(x, 0, 0.4), 
+#' n1 = 100, n2 = 100, observed_t = 3, lty = c(1, 1, 2))
+#'
+#' @export
+#' 
+visualize_predictions <- function(alternative = function(x) dcauchy(x, scale = sqrt(2) / 2),
+                                  n1, n2, from = -6, to = 6,
+                                  lty = c(1, 1, 2), lwd = 3,
+                                  col = c("#005A31", "red", "black", "orange"),
+                                  observed_t = NULL, frame.plot = FALSE,
+                                  xlab = "t-value", BFx = from + 0.1,
+                                  BFy = 0.1, BF10 = TRUE,
+                                  legend_placement = "topleft", ...) {
+  ## 1. Draw null predictions
+  nullhyp <- function(x) dt(x, n1 + n2 - 2)
+  curve(nullhyp, from = from , to = to, col = col[1], lty = lty[1], lwd = lwd[1],
+        frame.plot = frame.plot, yaxt = "n", ylab = "", xlab = xlab, ...)
+  maxdensity <- nullhyp(0)
+  ## 2. Draw alternative hypothesis predictions
+  if (!is.null(alternative)) {
+    predictions_alt <- sample_likelihoods(alternative, n1, n2, from, to)
+    points(predictions_alt$x, predictions_alt$y, type = "l", col = col[2],
+           lty = lty[2], lwd = lwd)
+    legend(legend_placement, lwd = 3, legend = c("Null", "Alternative"), 
+           cex = 1.2, col = c("#005A31", "red"), lty = c(2,2), bty = "n")
+    maxdensity <- max(maxdensity, predictions_alt$y)
+  }
+  ## 3. Illustrate the observed effect and the Bayes factor
+  if (!is.null(observed_t)) {
+    null_likelihood <- nullhyp(observed_t)
+    lines(x = rep(observed_t, 2), y = c(0, maxdensity), 
+          col = col[3], lwd = lwd, lty = lty[3])
+    points(observed_t, null_likelihood, pch = 19, col = col[4], cex = 2)
+    if (!is.null(alternative)) {
+      alt_likelihood <- marginal_likelihood(observed_t, alternative, n1, n2)
+      BF <- alt_likelihood / null_likelihood
+      displayBF <- "BF10 = "
+      if (BF10 == FALSE) {
+        BF <- 1 / BF
+        displayBF <- "BF01 = "
+      }
+      points(observed_t, alt_likelihood, pch = 19, col = col[4], cex = 2)
+      max_likelihood <- max(null_likelihood, alt_likelihood)
+      text(x = BFx, y = BFy, pos = 4, 
+           labels = paste(displayBF, round(BF, 2)))
+    }
+  }
+}
+
+#' Generate multiple marginal likelihoods in a given interval of
+#' t-values
+#' 
+#' @param alternative A function object. The default is a Cauchy prior
+#'     with scaling parameter `sqrt(2) / 2` as is the default in package
+#'     `BayesFactor` (Morey & Rouder, 2015). This argument can also be a
+#'     scalar number, in which case it is assumed that the alternative
+#'     is a point hypothesis on Cohen's d (with Cohen's d = `prior`).
+#' @param n1 The sample size in group 1
+#' @param n2 The sample size in group 2
+#' @param from The lowest t-value
+#' @param to The largest t-value
+#' 
+#' @return A data.frame of two columns: Column x contains the t-values, 
+#'   column y contains the marginal likelihoods
+#'
+#'
+#' @examples
+#' sample_likelihoods(n1 = 30, n2 = 30, from = -3, to = 3)
+#' sample_likelihoods(function(x) dnorm(x, 0, 0.3), n1 = 30, n2 = 30)
+#' 
+#' @export
+#' 
+sample_likelihoods <- function(alternative = function(x) dcauchy(x, scale = sqrt(2) / 2), 
+                               n1, n2, from = -6, to = 6) {
+  observed_ts <- seq(from, to, .01) # hypothetical observed t-values
+  ## Allow for a point null hypothesis
+  if (class(alternative) != "function") {
+    althyp <- function(x) dt(x, df = n1 + n2 - 2, ncp = ncp_from_d(alternative, n1, n2))
+    predictions <- althyp(observed_ts)
+    return(data.frame(x = observed_ts, y = predictions))
+  }
+  predictions <- vector(length = length(observed_ts)) # predict marginal likelihood of each t
+  for (i in 1:length(predictions)) {
+    predictions[i] <- marginal_likelihood(observed_ts[i], alternative, n1, n2)
+  }
+  return(data.frame(x = observed_ts, y = predictions))
+}
+
+#' Compute the marginal likelihood of an observed t-value given a
+#' hypothesis
+#' 
+#' This function can be used to compute a Bayes factor as the ratio
+#' of two marginal likelihoods.
+#' 
+#' @param observed_t The observed t-value.
+#' @param prior A function object describing the a-priori plausibility
+#'     of all effect sizes. The default is a Cauchy prior as in package
+#'     BayesFactor (Morey & Rouder, 2015). Can also be a scalar, in
+#'     which case it is assumed that the alternative is a point
+#'     hypothesis on Cohen's d (with value `prior`).
+#' @param n1 The sample size in group 1
+#' @param n2 The sample size in group 2
+#'
+#' @return The marginal likelihood of the observed t-value given the
+#'     prior on Cohen's d.
+#'
+#' @examples
+#' ## Standard Cauchy prior:
+#' marginal_likelihood(3, n1 = 30, n2 = 30)
+#' ## Normal prior - N(0, 0.3)
+#' marginal_likelihood(3, function(x) dnorm(x, 0, 0.3), n1 = 30, n2 = 30)
+#'
+#' @export
+#' 
+marginal_likelihood <- function(observed_t, 
+                                prior = function(x) dcauchy(x, scale = sqrt(2) / 2), 
+                                n1, n2) {
+  ## allow for point alternative:
+  if (class(prior) != "function") {
+    althyp <- function(x) dt(x, df = n1 + n2 - 2, ncp = ncp_from_d(prior, n1, n2))
+    return(althyp(observed_t))
+  }
+  integrate(weighted_likelihood, lower = -Inf, upper = Inf, 
+            observed_t = observed_t, prior = prior, n1 = n1, n2 = n2)$value
+}
+
+#' Compute the weighted likelihood of an observed t-value
+#' 
+#' For an assumed true effect size (Cohen's d), compute the likelihood
+#' of an observed t-value weighted by the a priori plausbility of this
+#' effect size. This function is primarily used to compute the marginal 
+#' likelihood of the effect size given a continuous hypothesis (e.g., 
+#' a Cauchy prior) by integrating in function `marginal_likelihood`
+#' below.
+#' 
+#' @param true_d A one-element vector, representing an assumed true 
+#'   effect size d, conditioned on which the likelihood is computed.
+#' @param observed_t The observed t-value.
+#' @param prior A function object describing the a-priori plausibility
+#'   of all effect sizes. The default is a cauchy prior
+#'   as in package BayesFactor (Morey & Rouder, 2015)
+#' @param n1 The sample size in group 1
+#' @param n2 The sample size in group 2
+#'
+#' @return The weighted likelihood of the observed t-value.
+
+weighted_likelihood <- function(true_d, observed_t, 
+                                prior = function(x) dcauchy(x, scale = sqrt(2) / 2), 
+                                n1, n2) {
+  dfr <- n1 + n2 - 2 ## degrees of freedom for two-sample t-test
+  ncp = ncp_from_d(true_d, n1, n2)
+  return(dt(observed_t, dfr, ncp) * prior(true_d))
+}
+
+                     
+#' Compute the non-centrality parameter for non-central t distribution
+#' from Cohen's d
+#' 
+#' @param d Cohen's d
+#' @param n1 Sample size in group 1
+#' @param n2 Sample size in group 2
+#' 
+#' @details See page 7 in Erdfelder, Faul, & Buchner (1996)
+#' 
+#' @return The non-centrality parameter
+#' 
+#' @references 
+#' Erdfelder, E., Faul, F., & Buchner, A. (1996). GPOWER: A general 
+#' power analysis program. Behavior research methods, instruments, & 
+#' computers, 28(1), 1-11.
+#'
+#' @examples
+#' ncp_from_d(0.3, 50, 50)
+#'
+#' @export
+#' 
+ncp_from_d <- function(d, n1, n2) {
+  ncp <- d * sqrt((n1 * n2^2 + n2 * n1^2) / ((n1 + n2)^2))
+  return(ncp)
+}
